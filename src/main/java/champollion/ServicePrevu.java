185b98e1ba9cb227c2aa91d13a0889ceff1bdb52
Hoarau Julie <Hoarau Julie@DESKTOP-N5V4GFF>
2020-11-19 16:22:00 +0100

    Final TP Stationnements

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -1,6 +1,33 @@
 package champollion;
 
-public class ServicePrevu {
-	// TODO : implémenter cette classe
+public class ServicePrevu 
+{ 
+    private final int volumeTD;
+    private final int volumeTP;
+    private final int volumeCM;
+    private final UE ue; 
+  
 
+    public ServicePrevu(int volumeTD,int volumeTP,int volumeCM,UE ue)
+    {  this.volumeTD= volumeTD;
+       this.volumeCM = volumeCM;
+       this.volumeTP = volumeTP;
+       this.ue = ue;
+    }
+    
+    public int getvolumeCM()
+    {  return volumeCM;
+    }
+
+    public int getvolumeTD()
+    {  return volumeTD;
+    }
+
+    public int getvolumeTP()
+    {  return volumeTP;
+    }
+    
+    public UE getue()
+    {   return ue;
+    } 
 }
