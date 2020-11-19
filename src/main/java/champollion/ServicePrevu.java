253e17d67ae6b24b24053ddf3afe62029f4ec555
Hoarau Julie <Hoarau Julie@DESKTOP-N5V4GFF>
2020-11-19 17:50:35 +0100

    Final TP Stationnements

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -6,13 +6,15 @@ public class ServicePrevu
     private final int volumeTP;
     private final int volumeCM;
     private final UE ue; 
+    private final Enseignant Enseignant;
   
 
-    public ServicePrevu(int volumeTD,int volumeTP,int volumeCM,UE ue)
+    public ServicePrevu(int volumeTD,int volumeTP,int volumeCM,UE ue,Enseignant Enseignant)
     {  this.volumeTD= volumeTD;
        this.volumeCM = volumeCM;
        this.volumeTP = volumeTP;
        this.ue = ue;
+       this.Enseignant = Enseignant;
     }
     
     public int getvolumeCM()
@@ -30,4 +32,8 @@ public class ServicePrevu
     public UE getue()
     {   return ue;
     } 
+    public Enseignant getEnseignant()
+    {   return Enseignant;
+    } 
 }
+
